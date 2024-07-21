e1767113c77a7516e5d8198a28d2e7ba9fa29efe
Jeisson <jeissonperez1@gmail.com>
2024-07-20 23:45:30 -0500

    cambios de la seccion 5 terminados, cuadrar detalles

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,9 +15,23 @@ module.exports = {
       backgroundColor:{
         "background-primera":"#fdf2e9",
         "backgrouund-tag-verde": "#51cf66"
+      },
+      animation:{
+        'show-card-icon': 'showCardIcon 300ms linear'
+      },
+      keyframes:{
+        showCardIcon:{
+          'from':{ transform: 'translateY(-200%)'},
+          'to': { transform: 'translateY(0%)'},
+        },
       }
       
     }
+  },
+  variants:{
+    animation:['responsive','hover','group-hover'],
+    animate:['responsive','hover','group-hover'],
+    fontSize:['responsive','hover','group-hover']
   },
   plugins: [],
 }
